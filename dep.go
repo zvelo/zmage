2f812416e7397ddc0c6f07ffe00121f884051365
Joshua Rubin <jrubin@zvelo.com>
2018-01-25 15:16:26 -0700

    add dep

---
+++ dep.go
@@ -0,0 +1,24 @@
+package zmage
+
+import (
+	"context"
+
+	"github.com/magefile/mage/sh"
+)
+
+func Dep(ctx context.Context) error {
+	modified, err := Modified("./Gopkg.lock", "./Gopkg.toml")
+	if err != nil {
+		return err
+	}
+
+	if !modified {
+		return nil
+	}
+
+	if err = sh.Run("dep", "ensure"); err != nil {
+		return err
+	}
+
+	return sh.Run("dep", "prune")
+}
